6c750721de07d59a5aca8015383632ddb7715931
normanrs <normanrs@live.com>
2018-11-27 18:44:33 -0700

    working items with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_233332) do
+ActiveRecord::Schema.define(version: 2018_11_28_012743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_11_27_233332) do
   create_table "customers", force: :cascade do |t|
     t.citext "first_name"
     t.citext "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.citext "name"
+    t.string "description"
+    t.integer "unit_price"
+    t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+describe 'items API' do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    results = JSON.parse(response.body)
+    expect(results.count).to eq(3)
+  end
+
+  it "sends one item by id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result["id"]).to eq(id)
+  end
+
+  it "sends one item at random" do
+    create_list(:item, 3)
+
+    get "/api/v1/items/random"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result.class).to eq(Hash)
+  end
+
+  it "finds one item by case_insensitive name" do
+    test1 = create(:item, name: "Norm")
+    test2 = create(:item, name: "Dave")
+    get "/api/v1/items/find?name=#{test2.name.downcase}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result["id"]).to eq(test2.id)
+  end
+
+  it "finds one item by id" do
+    test1 = create(:item)
+    test2 = create(:item)
+    get "/api/v1/items/find?id=#{test2.id}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result["id"]).to eq(test2.id)
+  end
+
+  it "finds one item by created_at" do
+    item = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+
+    get "/api/v1/items/find?created_at=#{item.created_at}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result["id"]).to eq(item.id)
+  end
+
+  it "finds one item by updated_at" do
+    item = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+
+    get "/api/v1/items/find?created_at=#{item.updated_at}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result["id"]).to eq(item.id)
+  end
+
+  it "finds all items by case_insensitive name" do
+    test1 = create(:item, name: "Norm")
+    test2 = create(:item, name: "Norm")
+    get "/api/v1/items/find_all?name=#{test2.name.downcase}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result.count).to eq(2)
+  end
+
+  it "finds all items by created_at" do
+    item1 = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+    item2 = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+    item3 = Item.create!(created_at: "2018-08-02 09:00:00 UTC", updated_at: "2018-08-02 09:00:00 UTC")
+
+    get "/api/v1/items/find_all?created_at=#{item1.created_at}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result.count).to eq(2)
+  end
+
+  it "finds all items by updated_at" do
+    item1 = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+    item2 = Item.create!(created_at: "2018-08-01 09:00:00 UTC", updated_at: "2018-08-01 09:00:00 UTC")
+    item3 = Item.create!(created_at: "2018-08-02 09:00:00 UTC", updated_at: "2018-08-02 09:00:00 UTC")
+
+    get "/api/v1/items/find_all?created_at=#{item1.updated_at}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result.count).to eq(2)
+  end
+
+  it "finds all items by id" do
+    item1 = Item.create!
+    item2 = Item.create!
+    item3 = Item.create!
+
+    get "/api/v1/items/find_all?id=#{item1.id}"
+
+    result = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(result.count).to eq(1)
+  end
+
+end
