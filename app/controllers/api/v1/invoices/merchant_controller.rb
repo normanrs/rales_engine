0f88d1121943e90ae6646fcfd3e43524682727ef
normanrs <normanrs@live.com>
2018-11-30 15:48:26 -0700

    up to items functionality with passing tests

--- app/controllers/api/v1/invoices/merchant_controller.rb
+++ app/controllers/api/v1/invoices/merchant_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Invoices::MerchantController < ApplicationController
 
   def show
-    id_in =   id_in = Invoice.find(params[:id]).merchant_id
+    id_in = Invoice.find(params[:id]).merchant_id
     render json: MerchantSerializer.new(Merchant.find(id_in))
   end
 
