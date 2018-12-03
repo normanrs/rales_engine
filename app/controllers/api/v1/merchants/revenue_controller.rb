747d1d0d77d79c504ed3e5e6e6fa548030339fb4
normanrs <normanrs@live.com>
2018-12-02 18:00:24 -0700

    added reading and minor fixes

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,22 +3,24 @@ require 'ostruct'
 
   def show
     if params[:date] != nil
-      money_in = OpenStruct.new(:id => 1, :money => Merchant.revenue(search_params[:date]) )
+      money_in = OpenStruct.new(:id => 1,
+                                :money => Merchant.revenue(search_params[:date]) )
       render json: MoneySerializer.new(money_in)
     else
       merch = Merchant.find(search_params[:merchant_id])
-      merch_in = OpenStruct.new(:id => merch.id, :name => merch.name, :created_at => merch.created_at, :updated_at => merch.updated_at, :revenue => merch.money_made.to_i )
-      # money_in = OpenStruct.new(:id => merch.id, :money => (merch.money_made) )
+      merch_in = OpenStruct.new(:id => merch.id,
+                                :name => merch.name,
+                                :created_at => merch.created_at,
+                                :updated_at => merch.updated_at,
+                                :revenue => merch.money_made.to_i )
       render json: MerchantsumSerializer.new(merch_in)
     end
   end
-
 
 private
 
   def search_params
     params.permit(:id, :merchant_id, :date)
   end
-
 
 end
